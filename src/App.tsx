02fad042f3b4b5a5ba8633fde2e7820cd9917a74
Zawwar Ahmad <zawwarahmad1998@gmail.com>
2024-02-11 14:00:50 +0000

    v2

--- src/App.tsx
+++ src/App.tsx
@@ -17,14 +17,14 @@ export default function Page() {
       "$2000! mehr or no",
       "$5000!!! or no",
       "Second Last chance!",
-      "Surely not?",
-      "You might regret this!",
+      "1 flower?",
+      "2 flowers!",
       "Give it another thought!",
       "Are you absolutely certain?",
-      "This could be a mistake!",
+      "3 flowers!",
       "Have a heart!",
       "Don't be so cold!",
-      "Change of heart?",
+      "fine ill get you a bouquet nigga",
       "Wouldn't you reconsider?",
       "Is that your final answer?",
       "You're breaking my heart ;(",
@@ -39,7 +39,7 @@ export default function Page() {
         {yesPressed ? (
           <>
             <img src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif" />
-            <div className="text-container">Ok yay!!!</div>
+            <div className="text-container">Ok yay ILY!!!</div>
           </>
         ) : (
           <>
@@ -48,7 +48,7 @@ export default function Page() {
               style={{ width: "400x", height: "240px" }}
               src="https://gifdb.com/images/high/cute-love-bear-roses-ou7zho5oosxnpo6k.gif"
             />
-            <h1 className="text-container">Will you be my Valentine?</h1>
+            <h1 className="text-container">Will you be my Valentine Rabail?</h1>
             <div>
               <button
                 className={"yes-button"}
